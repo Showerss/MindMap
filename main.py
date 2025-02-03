349398c4b801a581acb3734dea49178e76488bdf
PBanky <pbanky@sandiego.edu>
2025-02-03 12:10:20 -0800

    creating a class to make a canvas, also swapped the layout a bit to left and right sections of my window.

--- main.py
+++ main.py
@@ -4,6 +4,29 @@
 
 #import gui functionality from the tkinter module
 from tkinter import *
+import tkinter as tk
+
+
+class MindmapCanvas(tk.Canvas):
+    def __init__(self):
+        super().__init__()
+        self.configure(background='white')
+
+        #create a canvas object to display the mindmap
+        self.canvas = Canvas(window, width=600, height=600, bg='white')
+        self.canvas.pack(side='right')
+
+        #
+        self.scan_mark_x = 0
+        self.scan_mark_y = 0
+
+        #bind events to the mouse to allow the user to move the canvas around
+        self.canvas.bind('<ButtonPress-1>', self.start_pan)
+        self.canvas.bind('<B1-Motion>', self.pan)
+        self.canvas.bind('<MouseWheel>', self.zoom)
+
+        #initial scale factor of zoom
+        self.scale_factor = 1.0
 
 ############# Functions #############
 def how_to_popup():
@@ -25,42 +48,70 @@ def show_contact_window():
 
 
 
+def create_map():
+    pass
+
+
+
 ############# Main Program Window #############
 
 # Create a window object
 window = Tk()
 
 # Set the window title
-window.title('MainWindow')
+window.title('Safri Mindmap')
 
 # Set the window size
 window.geometry('800x600')
 
+#set window color
+window.configure(bg='light blue')
 
-#display some buttons to generate a blank page on the right hand side of the window
-newButton = Button(window, text='New')
-newButton.grid(column=1, row=0)
+# create an output frame on the right 2/3 of the window
+output_frame = Frame(window, width=600, height=600, bg='white')
+output_frame.pack(side='right')
 
-#how-to button
-howToButton = Button(window, text='How to use')
-howToButton.grid(column=1, row=1)
+#buttons frame on the left 1/3 of the window
+buttons_frame = Frame(window, width=200, height=600, bg='light blue')
+buttons_frame.pack(side='left')
+
+#display some buttons to generate a blank page, anchor it to the buttons_frame
+newButton = Button(buttons_frame, text='New')
+newButton.pack(side='top', anchor='nw')
+
+#how-to button located at the bottom right of the window, to the left of the contact button
+howToButton = Button(buttons_frame, text='How to use')
+# howToButton.pack(side='bottom', anchor='se')
 howToButton.config(command=how_to_popup)
 
 #create button
-createButton = Button(window, text='Create')
-createButton.grid(column=1, row=2)
+createButton = Button(buttons_frame, text='Create')
+# createButton.grid(row=0, column=0, sticky='nw')
 createButton.config(command=create_map)
 
 
 #contact button
-contactButton = Button(window, text='Contact Us')
-contactButton.grid(column=1, row=3)
+contactButton = Button(buttons_frame, text='Contact Us')
+contactButton.pack(side='bottom', anchor='sw')
 contactButton.config(command=show_contact_window)
 
 
+mindmap_canvas = MindmapCanvas(output_frame, width=600, height=600, bg='white')
+mindmap_canvas.pack(fill='both', expand=True)
 
+
+#TODO: create a canvas object to display the mindmap
+#TODO: create an import button that allows you to bring in a .docx file and convert it to a mindmap
+#TODO: have the user create the central idea of the mindmap so that we know what to build off of in the imported file 
+#TODO: create a function to read over the .docx file and either count the spacing or the bullet points to determine the hierarchy of the mindmap,
 
 
 #display the window
 window.mainloop()
+
+
+
+#Question: does it make more sense to have a c# that calls python? or a python that calls c#?
+#Question: how do I create a mindmap?
+
 
