d09dba8644bb9bc639f2e096497022fd950af042
PBanky <pbanky@sandiego.edu>
2025-02-03 12:29:14 -0800

    ooooo figuring stuff out about tk() haven't used this in like a year since my 120 class so its nice to refresh myself

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 #mindmap program by Phillip Banky
 #This program is a simple mindmap program that allows the user to create a mindmap
 
-
-#import gui functionality from the tkinter module
-from tkinter import *
 import tkinter as tk
 
 
 class MindmapCanvas(tk.Canvas):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        #set the background color of the canvas
         self.configure(background='white')
 
         #create a canvas object to display the mindmap
-        self.canvas = Canvas(window, width=600, height=600, bg='white')
+        self.canvas = tk.Canvas(window, width=600, height=600, bg='white')
         self.canvas.pack(side='right')
 
         #
@@ -25,12 +24,23 @@ class MindmapCanvas(tk.Canvas):
         self.canvas.bind('<B1-Motion>', self.pan)
         self.canvas.bind('<MouseWheel>', self.zoom)
 
+
         #initial scale factor of zoom
         self.scale_factor = 1.0
 
+    #create my methods for moving around the canvas
+    def start_pan(self, event):
+        pass
+
+    def pan(self, event):
+        pass
+
+    def zoom(self, event):
+        pass
+
 ############# Functions #############
 def how_to_popup():
-    how_to_window = Tk()
+    how_to_window = tk.Tk()
     how_to_window.title('How to use Mindmap')
     how_to_window.geometry('800x600')
     how_to_window.mainloop()
@@ -41,22 +51,16 @@ def create_map():
 
 
 def show_contact_window():
-    contact_window = Tk()
+    contact_window = tk.Tk()
     contact_window.title('Contact Us')
     contact_window.geometry('800x600')
     contact_window.mainloop()
 
 
-
-def create_map():
-    pass
-
-
-
 ############# Main Program Window #############
 
 # Create a window object
-window = Tk()
+window = tk.Tk()
 
 # Set the window title
 window.title('Safri Mindmap')
@@ -68,30 +72,30 @@ window.geometry('800x600')
 window.configure(bg='light blue')
 
 # create an output frame on the right 2/3 of the window
-output_frame = Frame(window, width=600, height=600, bg='white')
+output_frame = tk.Frame(window, width=600, height=600, bg='white')
 output_frame.pack(side='right')
 
 #buttons frame on the left 1/3 of the window
-buttons_frame = Frame(window, width=200, height=600, bg='light blue')
+buttons_frame = tk.Frame(window, width=200, height=600, bg='light blue')
 buttons_frame.pack(side='left')
 
 #display some buttons to generate a blank page, anchor it to the buttons_frame
-newButton = Button(buttons_frame, text='New')
+newButton = tk.Button(buttons_frame, text='New')
 newButton.pack(side='top', anchor='nw')
 
 #how-to button located at the bottom right of the window, to the left of the contact button
-howToButton = Button(buttons_frame, text='How to use')
+howToButton = tk.Button(buttons_frame, text='How to use')
 # howToButton.pack(side='bottom', anchor='se')
 howToButton.config(command=how_to_popup)
 
 #create button
-createButton = Button(buttons_frame, text='Create')
+createButton = tk.Button(buttons_frame, text='Create')
 # createButton.grid(row=0, column=0, sticky='nw')
 createButton.config(command=create_map)
 
 
 #contact button
-contactButton = Button(buttons_frame, text='Contact Us')
+contactButton = tk.Button(buttons_frame, text='Contact Us')
 contactButton.pack(side='bottom', anchor='sw')
 contactButton.config(command=show_contact_window)
 
@@ -110,8 +114,6 @@ mindmap_canvas.pack(fill='both', expand=True)
 window.mainloop()
 
 
-
-#Question: does it make more sense to have a c# that calls python? or a python that calls c#?
 #Question: how do I create a mindmap?
 
 
